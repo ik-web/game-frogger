6cb870345b98512eb0089476f67b1c88d1d7c38b
Ihor Kuchin <vedunigor@gmail.com>
2022-09-25 13:50:27 +0300

    Edited code

--- scripts/app.js
+++ scripts/app.js
@@ -4,20 +4,20 @@ const indentBorderTop = 3;
 const indentBorderRight = 404;
 const indentBorderBottom = 370;
 const levelForWin = 12;
-// ---------------------------------------------
-const playerSprite = "images/char-horn-girl.png";
+
+const playerSprite = "img/char-horn-girl.png";
 const playerStartLevel = 1;
 const playerStartPosX = 205;
 const playerStartPosY = 380;
 const playerStepX = 101;
 const playerStepY = 80;
-// ---------------------------------------------
+
 const borderCollision = 50;
-// ---------------------------------------------
-const enemySprite = "images/enemy-bug.png";
+
+const enemySprite = "img/enemy-bug.png";
 const enemySpeedMax = 500;
 const enemySpeedMin = 150;
-//----------------------------------------------
+
 const enemyPosX = -100;
 const enemyPosY = function () {
   const initYPositions = [60, 140, 220];
@@ -25,18 +25,15 @@ const enemyPosY = function () {
 
   return initYPositions[randY];
 };
-//----------------------------------------------
-const levelCounter = document.createElement("div");
-//----------------------------------------------
 
-// Generate enemies' speed
+const levelCounter = document.createElement("div");
+
 function enemySpeed() {
   return Math.floor(
     Math.random() * (enemySpeedMax - enemySpeedMin) + enemySpeedMin
   );
 }
 
-// Add a new enemy after level up
 function addEnemy() {
   let enemy = new Enemy(
     enemyPosX,
@@ -48,57 +45,47 @@ function addEnemy() {
   allEnemies.push(enemy);
 }
 
-// Reset enemy if the player won or lose
 function resetEnemy() {
   allEnemies = [];
   addEnemy();
 }
 
-// Reset game if the player won or lose
 function resetGame() {
   resetEnemy();
   player.level = playerStartLevel;
   setLevel(playerStartLevel);
 }
 
-//Create level counter
 function addLevelCounter() {
   levelCounter.style.cssText = `
-        position: fixed; 
-        top: 10px; 
-        left: 50%;
-        font-family: sans-serif;
-        font-size: 30px; 
-        transform: translateX(-50%);
+      position: fixed; 
+      top: 10px; 
+      left: 50%;
+      font-family: sans-serif;
+      font-size: 30px; 
+      transform: translateX(-50%);
     `;
 
   setLevel(playerStartLevel);
   document.body.prepend(levelCounter);
 }
 
-// Set level to counter
 function setLevel(level) {
   levelCounter.innerHTML = `Level: ${level}`;
 }
 
-//----------------------------------------------
-// Constructor for create character
 const Character = function (x, y, sprite) {
   this.x = x;
   this.y = y;
   this.sprite = sprite;
 };
 
-// Method for character update
 Character.prototype.update = function () {};
 
-// Draw the character on the screen, required method for game
 Character.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-//----------------------------------------------
-// Constructor for create enemy
 const Enemy = function (x, y, sprite, speed, player) {
   Character.call(this, x, y, sprite);
   this.speed = speed;
@@ -107,7 +94,6 @@ const Enemy = function (x, y, sprite, speed, player) {
 
 Enemy.prototype = Object.create(Character.prototype);
 
-//Check characters collision
 Enemy.prototype.enemyCollision = function () {
   if (
     this.x - borderCollision <= this.player.x && // Left
@@ -126,7 +112,6 @@ Enemy.prototype.enemyCollision = function () {
   }
 };
 
-// Method for enemies update
 Enemy.prototype.update = function (dt) {
   this.enemyCollision();
 
@@ -138,8 +123,6 @@ Enemy.prototype.update = function (dt) {
   }
 };
 
-//----------------------------------------------
-// Constructor for create player
 const Player = function (x, y, sprite) {
   Character.call(this, x, y, sprite);
   this.level = playerStartLevel;
@@ -147,7 +130,6 @@ const Player = function (x, y, sprite) {
 
 Player.prototype = Object.create(Character.prototype);
 
-//Method for checking game progress
 Player.prototype.checkProgress = function () {
   if (this.y < 0) {
     setTimeout(() => {
@@ -190,8 +172,6 @@ Player.prototype.handleInput = function (btn) {
   }
 };
 
-// ---------------------------------------------
-// This listens for key presses and sends the keys to Player.handleInput() method.
 document.addEventListener("keyup", function (e) {
   var allowedKeys = {
     37: "left",
@@ -202,15 +182,10 @@ document.addEventListener("keyup", function (e) {
 
   player.handleInput(allowedKeys[e.keyCode]);
 });
-// ---------------------------------------------
-// ---------------------------------------------
-// ---------------------------------------------
-// Create player character
+
 const player = new Player(playerStartPosX, playerStartPosY, playerSprite);
 
-// Create first enemy
 let allEnemies = [];
 addEnemy();
 
-// Add level counter
 addLevelCounter();
--- scripts/engine.js
+++ scripts/engine.js
@@ -107,12 +107,12 @@ var Engine = (function (global) {
      * for that particular row of the game level.
      */
     var rowImages = [
-        "images/water-block.png", // Top row is water
-        "images/stone-block.png", // Row 1 of 3 of stone
-        "images/stone-block.png", // Row 2 of 3 of stone
-        "images/stone-block.png", // Row 3 of 3 of stone
-        "images/grass-block.png", // Row 1 of 2 of grass
-        "images/grass-block.png", // Row 2 of 2 of grass
+        "img/water-block.png", // Top row is water
+        "img/stone-block.png", // Row 1 of 3 of stone
+        "img/stone-block.png", // Row 2 of 3 of stone
+        "img/stone-block.png", // Row 3 of 3 of stone
+        "img/grass-block.png", // Row 1 of 2 of grass
+        "img/grass-block.png", // Row 2 of 2 of grass
       ],
       numRows = 6,
       numCols = 5,
@@ -170,12 +170,12 @@ var Engine = (function (global) {
    * all of these images are properly loaded our game will start.
    */
   Resources.load([
-    "images/stone-block.png",
-    "images/water-block.png",
-    "images/grass-block.png",
-    "images/enemy-bug.png",
-    "images/char-boy.png",
-    "images/char-horn-girl.png",
+    "img/stone-block.png",
+    "img/water-block.png",
+    "img/grass-block.png",
+    "img/enemy-bug.png",
+    "img/char-boy.png",
+    "img/char-horn-girl.png",
   ]);
   Resources.onReady(init);
 
